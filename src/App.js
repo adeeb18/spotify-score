8447f6e34494b617a37d8a85c6df216cdbf6c907
Lukas Vaiciunas <lvjuodasuo@gmail.com>
2023-03-22 23:31:57 -0400

    implement routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Song from "./pages/Song";
+import Album from "./pages/Album";
+import About from "./pages/About";
+import ViewReviews from "./pages/ViewReviews";
+import CreateReview from "./pages/CreateReview";
+import Leaderboard from "./pages/Leaderboard";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/search" element={<Search/>}/>
+        <Route exact path="/song" element={<Song/>}/>
+        <Route path="/song/reviews" element={<ViewReviews/>}/>
+        <Route path="/song/create-review" element={<CreateReview/>}/>
+        <Route path="/album" element={<Album/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/leaderboard" element={<Leaderboard/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
